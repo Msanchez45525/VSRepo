642d3c470a99feec5ca23b511db54cc461e71f6b
Matt Sanchez <msanchez@tql.com>
2021-05-23 15:30:57 -0400

    Updated Hmwk

---
+++ TQL-Banking/CD.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TQL_Banking
+{
+    class CD : Account
+    {
+
+        public DateTime DepositDate { get; set; }
+        public DateTime WithdrawDate { get; set; }
+        public decimal APR { get; set; }
+        public int CDDuration { get; set; }
+
+        public override bool Deposit(decimal amount)
+        {
+            if (Balance > 0)
+            {
+                Console.WriteLine("Cannot make additional deposit");
+                return false;
+            }
+            DepositDate = DateTime.Now;
+            WithdrawDate = DepositDate.AddMonths(CDDuration);
+            return base.Deposit(amount);
+        }
+        public override bool Withdraw(decimal amount)
+        {
+            if(DateTime.Now< WithdrawDate)
+            {
+                Console.WriteLine($"Cannot withdraw until {WithdrawDate}");
+                return false;
+            }
+            var interest = Balance * (APR / 12.0m) * CDDuration;
+            base.Deposit(interest);
+            return base.Withdraw(amount);
+
+
+            
+        }
+        public CD (int CDDuration)
+        {
+            this.CDDuration = CDDuration;
+            switch (this.CDDuration)
+
+            {
+                case 12: APR = 0.01m;
+                    break;
+                case 24: APR = 0.02m;
+                    break;
+                case 36: APR = 0.03m;
+                    break;
+                case 48: APR = 0.04m;
+                    break;
+                case 60: APR = 0.04m;
+                    break;
+
+            }
+
+        }
+
+    }
+
+
+}
--- TQL-Banking/Program.cs
+++ TQL-Banking/Program.cs
@@ -4,21 +4,29 @@ namespace TQL_Banking {
     class Program {
         static void Main(string[] args) {
 
-            var sav1 = new Savings();
-            sav1.Deposit(1000);
-            sav1.PayInterest(3);
+            //var sav1 = new Savings();
+            //sav1.Deposit(1000);
+            //sav1.PayInterest(3);
 
-            var acct1 = new Account();
-            acct1.Deposit(500);
-            acct1.Withdraw(200);
-            acct1.Withdraw(600);
-            acct1.Deposit(-400);
-            Console.WriteLine($"Balance is {acct1.Balance}");
+            //var acct1 = new Account();
+            //acct1.Deposit(500);
+            //acct1.Withdraw(200);
+            //acct1.Withdraw(600);
+            //acct1.Deposit(-400);
+            //Console.WriteLine($"Balance is {acct1.Balance}");
 
-            var acct2 = new Account();
-            acct1.Transfer(1000, acct2);
-            Console.WriteLine($"Balance is {acct1.Balance}");
-            Console.WriteLine($"Balance is {acct2.Balance}");
+            //var acct2 = new Account();
+            //acct1.Transfer(1000, acct2);
+            //Console.WriteLine($"Balance is {acct1.Balance}");
+            //Console.WriteLine($"Balance is {acct2.Balance}");
+
+
+            var CD1 = new CD(24);
+            CD1.Deposit(5000);
+
+            Console.WriteLine($"Balance is {CD1.Balance}" );
+
+            CD1.Withdraw(1000); 
         }
     }
 }
